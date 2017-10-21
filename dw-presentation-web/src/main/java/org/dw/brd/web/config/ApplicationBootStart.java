ac6a24b657f2785bef2c49a633e634f8692a298a
irfan jaan <irfanjaan10@gmail.com>
2017-10-21 18:46:17 +0530

    Initial project setup

---
+++ dw-presentation-web/src/main/java/org/dw/brd/web/config/ApplicationBootStart.java
@@ -0,0 +1,14 @@
+package org.dw.brd.web.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBootStart {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationBootStart.class, args);
+
+  }
+
+}
